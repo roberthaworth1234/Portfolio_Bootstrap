eaecf15e066d7403ab562c9e633b5ae7283beefb
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-05-05 20:45:22 +0100

    Updates

---
+++ precache-manifest.f4763aa33620ca059961524b47bbed8c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9db4448a353f986aec4de64e585e429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1916ef0b772144189ed",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d8e5cdd81678b4a578a3",
+    "url": "/static/css/main.c0be94f5.chunk.css"
+  },
+  {
+    "revision": "a1916ef0b772144189ed",
+    "url": "/static/js/2.f5b3eab5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f5b3eab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e5cdd81678b4a578a3",
+    "url": "/static/js/main.f341280e.chunk.js"
+  },
+  {
+    "revision": "e0fe6eb107d4e1dedaf9",
+    "url": "/static/js/runtime-main.3d300486.js"
+  },
+  {
+    "revision": "ec2882daded50483cd7e6d8c9c86a5a6",
+    "url": "/static/media/1.ec2882da.jpg"
+  },
+  {
+    "revision": "e840bb0c3bce645b9770d7d4b13914c3",
+    "url": "/static/media/10.e840bb0c.jpg"
+  },
+  {
+    "revision": "19e1e3038f73bf629dc21d82533a7fa6",
+    "url": "/static/media/2.19e1e303.jpg"
+  },
+  {
+    "revision": "48ab202f827b54682a9a9ca204643746",
+    "url": "/static/media/3.48ab202f.jpg"
+  },
+  {
+    "revision": "2504dcd2cb99fcb0be062db63f869f7e",
+    "url": "/static/media/4.2504dcd2.jpg"
+  },
+  {
+    "revision": "5cbd02d456431ad7787e5185ba6415aa",
+    "url": "/static/media/5.5cbd02d4.jpg"
+  },
+  {
+    "revision": "c5dbac0a2dbcff459e30a366ff5d166d",
+    "url": "/static/media/6.c5dbac0a.jpg"
+  },
+  {
+    "revision": "61c491b765bbbdc2be71add4bea2679c",
+    "url": "/static/media/7.61c491b7.jpg"
+  },
+  {
+    "revision": "32cac047c234e7acb199d82ab1bf8d11",
+    "url": "/static/media/8.32cac047.jpg"
+  },
+  {
+    "revision": "501c15f5259d1a6427c76025199a337b",
+    "url": "/static/media/9.501c15f5.jpg"
+  },
+  {
+    "revision": "a4287803318303755e3f73ec73f3bcc8",
+    "url": "/static/media/CV1.a4287803.png"
+  },
+  {
+    "revision": "a7dc44690f43bdf775ec29c33699c7cc",
+    "url": "/static/media/Rob_Haworth.a7dc4469.png"
+  },
+  {
+    "revision": "bfd7cf02f64368558ea0cfb8fb783d88",
+    "url": "/static/media/RobertHaworthCV.bfd7cf02.pdf"
+  },
+  {
+    "revision": "85aa6ded575047b2fe1289f2d9785085",
+    "url": "/static/media/RobertHaworthCV1.85aa6ded.pdf"
+  },
+  {
+    "revision": "273130b9e2dc775c3ff92253eb058d2e",
+    "url": "/static/media/Screenshot2.273130b9.png"
+  },
+  {
+    "revision": "8533635b563fbeffd5dc358efc0ec90b",
+    "url": "/static/media/Screenshot3.8533635b.png"
+  },
+  {
+    "revision": "779cba888d0328ae14737c3e78b6338f",
+    "url": "/static/media/Screenshot5.779cba88.png"
+  },
+  {
+    "revision": "1ac6f5f264989095ca81d186d74c74f6",
+    "url": "/static/media/Screenshot6.1ac6f5f2.png"
+  },
+  {
+    "revision": "19951bfba3e0293d7797f183a28a0a1f",
+    "url": "/static/media/Screenshot8.19951bfb.png"
+  },
+  {
+    "revision": "c714cd0cca017e252f367a432ee196d4",
+    "url": "/static/media/logoRH1.c714cd0c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio_Bootstrap/precache-manifest.f1840e6274eac91ce62469ecdd6b0d25.js"
+  "/precache-manifest.f4763aa33620ca059961524b47bbed8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio_Bootstrap/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
