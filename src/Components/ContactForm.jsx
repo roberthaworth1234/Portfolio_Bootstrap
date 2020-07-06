1223cd6ce611ec5548d760132cd489d548b2ba48
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-07-06 14:23:44 +0100

    updating contact form

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -2,85 +2,91 @@ import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import axios from "axios";
 
-const API_PATH = "http://localhost:1992/bootstrap-app/api/contact/index.php";
+const API_PATH = "http://localhost/bootstrap-app/api/contact/index.php";
 
 export default class ContactForm extends Component {
   state = {
     name: "",
     email: "",
-    message: "",
-    mailSent: false,
-    error: null
+    message: ""
   };
 
-  handleFormSubmit(event) {
-    event.preventDefault();
+  handleSubmit(e) {
+    e.preventDefault();
     axios({
-      method: "post",
-      url: `${API_PATH}`,
-      headers: { "content-type": "application/json" },
+      method: "POST",
+      url: "http://localhost:3002/send",
       data: this.state
-    })
-      .then(result => {
-        this.setState({
-          mailSent: result.data.sent
-        });
-      })
-      .catch(error => this.setState({ error: error.message }));
+    }).then(response => {
+      if (response.data.status === "success") {
+        alert("Message Sent.");
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        alert("Message failed to send.");
+      }
+    });
+  }
+
+  resetForm() {
+    this.setState({ name: "", email: "", message: "" });
   }
 
   render() {
     return (
-      <>
-        <h2 className="text-center mt-2">Contact Me</h2>
-        <div className="underline mb-2"></div>
-        <div>
-          <form className="d-flex flex-column align-items-center" action="#">
-            <label>Name</label>
+      <div className="App">
+        <form
+          id="contact-form"
+          onSubmit={this.handleSubmit.bind(this)}
+          method="POST"
+        >
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
             <input
-              className="w-50"
               type="text"
+              className="form-control"
               id="name"
-              name="name"
-              placeholder="Your name.."
               value={this.state.name}
-              onChange={e => this.setState({ name: e.target.value })}
+              onChange={this.onNameChange.bind(this)}
             />
-
-            <label>Email</label>
-            <input
-              className="w-50"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Your email.."
-              value={this.state.email}
-              onChange={e => this.setState({ email: e.target.value })}
-            />
-
-            <label>Message</label>
-            <textarea
-              className="w-50"
-              id="message"
-              name="message"
-              placeholder="Please write your query.."
-              onChange={e => this.setState({ message: e.target.value })}
-              value={this.state.message}
-            ></textarea>
-            <Button
-              className="my-2"
-              type="submit"
-              onClick={e => this.handleFormSubmit(e)}
-              value="Submit"
-            >
-              Submit
-            </Button>
-          </form>
-          <div>
-            {this.state.mailSent && <div>Thank you for contcting us.</div>}
           </div>
-        </div>
-      </>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              value={this.state.email}
+              onChange={this.onEmailChange.bind(this)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              className="form-control"
+              rows="5"
+              id="message"
+              value={this.state.message}
+              onChange={this.onMessageChange.bind(this)}
+            />
+          </div>
+          <Button type="submit" className="btn btn-primary">
+            Submit
+          </Button>
+        </form>
+      </div>
     );
+  }
+
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  onMessageChange(event) {
+    this.setState({ message: event.target.value });
   }
 }
